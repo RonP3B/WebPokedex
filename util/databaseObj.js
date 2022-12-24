363cff51af6535224cabbfff22ba6c04bd727fe2
Roniell Perez <roniell2016@hotmail.com>
2022-12-24 17:59:04 -0400

    Vars added

--- util/databaseObj.js
+++ util/databaseObj.js
@@ -1,5 +1,17 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_URI);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: true,
+    },
+  }
+);
 
 module.exports = sequelize;
