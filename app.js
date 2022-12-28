8de2e5db8a97418a59ce4af652a9c20876cb6d64
Roniell Perez <roniell2016@hotmail.com>
2022-12-28 00:53:14 -0400

    Authentication added

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 require("dotenv").config();
 const express = require("express");
 const { engine } = require("express-handlebars");
+const session = require("express-session")
 const multer = require("multer");
-const equal = require("./util/helpers/hbs/equal");
+const flash = require("connect-flash");
 const path = require("path");
-const sequelize = require("./util/databaseObj");
-const Pokemon = require("./models/Pokemon");
-const PokemonType = require("./models/PokemonType");
-const Region = require("./models/Region");
-const imgStorage = require("./util/imgStorage")
-const { home, pokemon, regions, types, notFound } = require("./routes/index");
+
+const { Pokemon, PokemonType, Region, User } = require("./exports/models")
+const { auth, home, pokemon, regions, types, notFound } = require("./exports/routes");
+const { sequelize, imgStorage } = require("./exports/utils");
+const { locals, addReqUser, isUnauthorized } = require("./exports/middlewares");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,23 +22,30 @@ app.engine(
     layoutsDir: "views/layouts",
     defaultLayout: "pokedex-layout",
     extname: "hbs",
-    helpers: { equal },
+    helpers: { equal: (a, b) => a === b, json: (obj) => JSON.stringify(obj) },
   })
 );
+
 app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(multer({ storage: imgStorage }).single("image"));
-app.use(home);
-app.use("/admin-pokemon", pokemon);
-app.use("/admin-regions", regions);
-app.use("/admin-types", types);
+app.use(session({ secret: "mysecret", resave: true, saveUninitialized: false }));
+app.use(flash());
+app.use(addReqUser);
+app.use(locals);
+app.use(auth);
+app.use("/pokemon", isUnauthorized, home);
+app.use("/admin-pokemon", isUnauthorized, pokemon);
+app.use("/admin-regions", isUnauthorized, regions);
+app.use("/admin-types", isUnauthorized, types);
 app.use(notFound);
 
 Pokemon.belongsTo(PokemonType, { constraint: true, onDelete: "CASCADE" });
 Pokemon.belongsTo(Region, { constraint: true, onDelete: "CASCADE" });
+
 PokemonType.hasMany(Pokemon);
 Region.hasMany(Pokemon);
 
